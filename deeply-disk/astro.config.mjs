84f174975434c7603decc5d57f26029dc5d5a94d
[ailyncodes] <[95152597+ailyncodes@users.noreply.github.com>
2024-06-20 01:29:49 -0600

    commit new changes

--- deeply-disk/astro.config.mjs
+++ deeply-disk/astro.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export default defineConfig({
   integrations: [react()],
